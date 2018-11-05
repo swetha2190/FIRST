165c6d2b4542756ade588cad96595d93fe90580b
swetha2190 <43073957+swetha2190@users.noreply.github.com>
2018-11-05 07:40:22 +0530

    constructor file created

---
+++ Constructor.java
@@ -0,0 +1,21 @@
+
+public class Constructor
+{
+private int id;
+private String name;
+public void Constructor()
+{
+  this.id=id;
+  this.name=name;
+}
+public void play()
+{
+System.out.println("i am playing with constructor");
+}
+public static void main(String args[])
+{
+ Constructor constructor=new Constructor()
+ constructor.play();
+}
+}
+
